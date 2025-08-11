ac066cecc23dacc2964313ea8f8b7309dd7bda9d
unknown <kaniobed28@gmail.com>
2025-08-11 22:08:48 +0200

    Update event date for Sermon On the Mount and set custom favicon in index.html

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   // Sample data for upcoming events
   events = [
-    { title: 'Sermon On the Mount', date: 'June 1  - June 7, 2025', time: '10:00 AM', description: 'Join us for a spirit-filled worship experience.' },
+    { title: 'Sermon On the Mount', date: 'October 10  - October 12, 2025', time: '10:00 AM', description: 'Join us for a spirit-filled worship experience.' },
     { title: 'Saturday Worship Service', date: 'Every Saturday', time: '10:00 AM', description: 'Join us for a spirit-filled worship experience.' },
     { title: 'Prayer Night', date: 'June 5, 2025', time: '7:00 PM', description: 'Come together for a night of prayer and fellowship.' },
   ];
